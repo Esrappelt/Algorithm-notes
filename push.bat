0a5859c35cd304b380fa7a2ac9c0418ba24caeae
Esrappelt <1090822794@qq.com>
2019-04-04 17:22:00 +0800

    2019/04/04 17:21:52 更改二分查找

--- push.bat
+++ push.bat
@@ -1,7 +1,11 @@
+set d=%date:~0,10%
+set t=%time:~0,8%
+set times=%d% %t%
+
 echo current directory:%cd%
 git status 
 git add .
 set /p declation=commit message:
-git commit -m "%declation%"
+git commit -m "%times% %declation%"
 git push origin master
-pause
+exit
